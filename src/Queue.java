ebd8738a26cb0db7100ddf7729a209a3b24f928f
fuldendogan <fulden.dogan@gmail.com>
2023-03-22 17:21:06 +0300

    Algo. Homework. Step 2-updated Queue class

--- src/Queue.java
+++ src/Queue.java
@@ -1,86 +1,142 @@
+
 public class Queue {
-    private int size;
-    private int capacity;
-    private Node head;
-    private Node end;
+    private int rear, front; private Object[] elements;
+    Queue (int capacity) {
 
-    public Queue() {
-        size = 0;
-        capacity = 12;
+    elements = new Object[capacity];
+    rear = -1;
+    front = 0;
     }
-    // ali->Emre->Furkan
 
-    public void enqueue(Node data) {
-        if (isFull()) {
-            System.out.println("Queue is full");
-            return;
-        }
-        if(this.end == null){
-            this.head = data;
-            this.end = this.head;
-        }
+    void enqueue(Object data) {
+        if (isFull())
+            System.out.println("Queue overflow");
         else {
-            this.end.setNext(data);
-            this.end = data;
+            rear++;
+            elements [rear] = data;
         }
-        size++;
     }
 
-    public Node dequeue() {
+    Object dequeue() {
+        if (isEmpty())
+        {
+            System.out.println("Queue is empty");
+            return null;
+        }
+        else
+        {
+            Object retData = elements[front];
+            elements[front]= null;
+            front++;
+            return retData;
+        }
+    }
+    Object peek() {
         if (isEmpty()) {
             System.out.println("Queue is empty");
             return null;
         }
-        if (head == end){
-            end = null;
-        }
-
-        Node headNext = this.head.getNext();
-        head = headNext;
-        size--;
-        return headNext;
+        else
+            return elements [front];
     }
-
-    public Node peek() {
-        if (isEmpty()) {
-            System.out.println("Queue is empty");
-            return null;
-        }
-        if (head == end){
-            end = null;
-        }
-
-        return this.head.getNext();
+        boolean isEmpty() {
+        return rear < front;
     }
-
-    public boolean isFull() {
-        return size == capacity;
+        boolean isFull() {
+        return (rear + 1 == elements.length);
     }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    @Override
-    public String toString() {
-        return head.getData().toString();
-    }
-
-    public String deepToString() {
-
-        StringBuilder res = new StringBuilder();
-        res.append(head.getData());
-
-        Node cur = head;
-        while (null != (cur = cur.getNext())){
-            res.append(" ");
-            res.append(cur.getData());
-
-        }
-        return res.toString();
+        int size() {
+        return rear - front + 1;
     }
 }
+
+
+//
+//public class Queue {
+//    private int size;
+//    private int capacity;
+//    private Node head;
+//    private Node end;
+//
+//
+//
+//    public Queue() {
+//        size = 0;
+//        capacity = 12;
+//    }
+//    // ali->Emre->Furkan
+//
+//    public void enqueue(Node data) {
+//        if (isFull()) {
+//            System.out.println("Queue is full");
+//            return;
+//        }
+//        if(this.end == null){
+//            this.head = data;
+//            this.end = this.head;
+//        }
+//        else {
+//            this.end.setNext(data);
+//            this.end = data;
+//        }
+//        size++;
+//    }
+//
+//    public Node dequeue() {
+//        if (isEmpty()) {
+//            System.out.println("Queue is empty");
+//            return null;
+//        }
+//        if (head == end){
+//            end = null;
+//        }
+//
+//        Node headNext = this.head.getNext();
+//        head = headNext;
+//        size--;
+//        return headNext;
+//    }
+//
+//    public Node peek() {
+//        if (isEmpty()) {
+//            System.out.println("Queue is empty");
+//            return null;
+//        }
+//        if (head == end){
+//            end = null;
+//        }
+//
+//        return this.head.getNext();
+//    }
+//
+//    public boolean isFull() {
+//        return size == capacity;
+//    }
+//
+//    public int size() {
+//        return size;
+//    }
+//
+//    public boolean isEmpty() {
+//        return size == 0;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return head.getData().toString();
+//    }
+//
+//    public String deepToString() {
+//
+//        StringBuilder res = new StringBuilder();
+//        res.append(head.getData());
+//
+//        Node cur = head;
+//        while (null != (cur = cur.getNext())){
+//            res.append(" ");
+//            res.append(cur.getData());
+//
+//        }
+//        return res.toString();
+//    }
+//}
