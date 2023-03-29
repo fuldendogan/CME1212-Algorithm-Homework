943a06266c04f40f7f6fffb0e249bab69a4c9989
fuldendogan <fulden.dogan@gmail.com>
2023-03-29 23:43:14 +0300

    Algo. Homework. Step 2-updated Queue class

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    static Queue nameQ = new Queue(30);   // HighScore names
-    static Queue scoreQ = new Queue(30);  // HighScore scores
-    static Stack player1 = new Stack(); // Player 1's cards
-    static Stack player2 = new Stack(); // Player 2's cards
+    static Queue nameQ = new Queue(12);   // HighScore names maksimum 12
+    static Queue scoreQ = new Queue(12);  // HighScore scores maksimum 12
+//    static Stack player1 = new Stack(); // Player 1's cards
+//    static Stack player2 = new Stack(); // Player 2's cards
 
     public static void main(String[] args) {
         try {
             File myObj = new File("highscoretable.txt");
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNext()) {
-                nameQ.enqueue(new Node(myReader.next()));
-                scoreQ.enqueue(new Node(myReader.next()));
+                nameQ.enqueue(new String(myReader.next()));
+                scoreQ.enqueue(new String(myReader.next()));
             }
+            sortQueue(nameQ, scoreQ);
             System.out.println("q1: " + nameQ);
-            System.out.println("q2: " + scoreQ.deepToString());
-//            sortQueue();
+            System.out.println("q2: " + scoreQ);
 //            System.out.println("sorted q1: " + nameQ.deepToString());
 //            System.out.println("sorted q2: " + scoreQ.deepToString());
             myReader.close();
@@ -29,116 +29,163 @@ public class Main {
             e.printStackTrace();
         }
 
-        takeInput();
+//        takeInput();
     }
 
-    private static void takeInput() {
-        int stackSize = 7;
-        while (stackSize < 7 || stackSize > 10) {
-            System.out.println("Please enter n");
-            Scanner input = new Scanner(System.in);
-            stackSize = input.nextInt();
-        }
+//    private static void takeInput() {
+//        int stackSize = 7;
+//        while (stackSize < 7 || stackSize > 10) {
+//            System.out.println("Please enter n");
+//            Scanner input = new Scanner(System.in);
+//            stackSize = input.nextInt();
+//        }
 
-        Random rand = new Random();
-        for (int j = 1; j <= stackSize; j++) {
-            while (player1.size() < stackSize) {
-                int num = rand.nextInt(13) + 1; // TODO: do not use magic numbers
-                if (!isStackContains(player1,num)) {
-                    player1.push(new Node(num));
-                }
-            }
-        }
+//        Random rand = new Random();
+//        for (int j = 1; j <= stackSize; j++) {
+//            while (player1.size() < stackSize) {
+//                int num = rand.nextInt(13) + 1; // TODO: do not use magic numbers
+//                if (!isStackContains(player1,num)) {
+//                    player1.push(new Node(num));
+//                }
+//            }
+//        }
 
-        for (int j = 1; j <= stackSize; j++) {
-            while (player2.size() < stackSize) {
-                int num = rand.nextInt(13) + 1;
-                if (!isStackContains(player2,num)) {
-                    player2.push(new Node(num));
-                }
-            }
-        }
-        boolean gameFinished = false;
-        int round = 1;
-        while (!gameFinished) {
-            gameFinished = gameLoop(round);
-            round++;
-        }
-        System.out.print("What is your name: ");
-        Scanner input = new Scanner(System.in);
-        Node name = new Node(input.nextLine());
+//        for (int j = 1; j <= stackSize; j++) {
+//            while (player2.size() < stackSize) {
+//                int num = rand.nextInt(13) + 1;
+//                if (!isStackContains(player2,num)) {
+//                    player2.push(new Node(num));
+//                }
+//            }
+//        }
+//        boolean gameFinished = false;
+//        int round = 1;
+//        while (!gameFinished) {
+//            gameFinished = gameLoop(round);
+//            round++;
+////        }
+//        System.out.print("What is your name: ");
+//        Scanner input = new Scanner(System.in);
+//        Node name = new Node(input.nextLine());
+//
+//        nameQ.enqueue(new Node(name));//TODO: ask: is this name already in the queue?
+//        scoreQ.enqueue(new Node(round)); //TODO: ask: is this score better than the first 12 scores?
 
-        nameQ.enqueue(new Node(name));//TODO: ask: is this name already in the queue?
-        scoreQ.enqueue(new Node(round)); //TODO: ask: is this score better than the first 12 scores?
 
 
-    }
 
-    private static boolean isStackContains(Stack stack, Object element) {
-        for (int i = 0; i < stack.size(); i++) {
-            if (getItemFromStack(stack,i) != null && getItemFromStack(stack,i) == element) {
-                return true;
-            }
-        }
-        return false;
-    }
+//    private static boolean isStackContains(Stack stack, Object element) {
+//        for (int i = 0; i < stack.size(); i++) {
+//            if (getItemFromStack(stack,i) != null && getItemFromStack(stack,i) == element) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
-    private static Node getItemFromStack(Stack stack, int i) {
-        //get i.th element from stack
-        Stack tempStack = new Stack();
-        Object item = null;
-        for (int j = 0; j < stack.size(); j++) {
-            Object element = stack.pop();
-            if (j == i) {
-                item = element;
-            }
-            tempStack.push(element);
-        }
-    }
+//    private static Node getItemFromStack(Stack stack, int i) {
+//        //get i.th element from stack
+//        Stack tempStack = new Stack();
+//        Object item = null;
+//        for (int j = 0; j < stack.size(); j++) {
+//            Object element = stack.pop();
+//            if (j == i) {
+//                item = element;
+//            }
+//            tempStack.push(element);
+//        }
+//    }
 
     //    q1: Kaan Ali Yeliz Cem Can Pelin Ece Sibel Ayşe Berk Ege Ada
 //q2: 130 100 120 140 100 110 140 150 120 160 120 80
     //Sort the queue q1 and q2 according to the scores in q2
-    private static void sortQueue() {
-        //todo: sort the queue according to the scores in q2
-    }
+//    private static void sortQueue() {
+//        //todo: sort the queue according to the scores in q2
+//    }
 
-    private static void sortStack(Stack stack) {
-        while (!stack.isEmpty()) {
-            Node max = stack.peek();
-            while (!stack.isEmpty()) {
-                Node temp = stack.pop();
-                if (Integer.parseInt(temp.getData().toString()) > Integer.parseInt(max.getData().toString())) {
-                    max = temp;
+    public static void sortQueue(Queue nameQ, Queue scoreQ) {
+        int size = scoreQ.size();
+
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                Object scoreObj = scoreQ.peek();
+                int score1;
+                if (scoreObj instanceof Integer) {
+                    score1 = (Integer) scoreObj;
                 } else {
-                    stack.push(temp);
+                    score1 = Integer.parseInt(scoreObj.toString());
+                }                scoreQ.dequeue();
+                String name1 = (String) nameQ.peek();
+                nameQ.dequeue();
+
+                Object scoreObj2 = scoreQ.peek();
+                int score2;
+                if (scoreObj instanceof Integer) {
+                    score2 = (Integer) scoreObj;
+                } else {
+                    score2 = Integer.parseInt(scoreObj.toString());
+                }                String name2 = (String) nameQ.peek();
+
+                if (score1 > score2) {
+                    scoreQ.enqueue(score1);
+                    nameQ.enqueue(name1);
+
+                    scoreQ.dequeue();
+                    nameQ.dequeue();
+
+                    scoreQ.enqueue(score2);
+                    nameQ.enqueue(name2);
+                } else {
+                    scoreQ.enqueue(score2);
+                    nameQ.enqueue(name2);
+
+                    scoreQ.dequeue();
+                    nameQ.dequeue();
+
+                    scoreQ.enqueue(score1);
+                    nameQ.enqueue(name1);
                 }
             }
-            stack.push(max);
         }
     }
 
-    private static boolean gameLoop(int round) {
-//        Player1: A 3 7 8 10 J K Score: 0 Bag1 A 2 3 4 5 6 7 8 9 10 J Q K
-//Player2: 2 3 5 6 8 9 Q Score: 0 Bag2
-//1. selected value: 3
-        System.out.print("Player1: ");
-        printStack(player1);
-        System.out.print("Player2: ");
-        printStack(player2);
-        return true;
-    }
 
-    private static void printStack(Stack stack) {
-        Stack tempStack = new Stack();
-        while (!stack.isEmpty()) {
-            Object element = stack.pop();
-            System.out.print(element + " ");
-            tempStack.push(element);
-        }
+//    private static void sortStack(Stack stack) {
+//        while (!stack.isEmpty()) {
+//            Node max = stack.peek();
+//            while (!stack.isEmpty()) {
+//                Node temp = stack.pop();
+//                if (Integer.parseInt(temp.getData().toString()) > Integer.parseInt(max.getData().toString())) {
+//                    max = temp;
+//                } else {
+//                    stack.push(temp);
+//                }
+//            }
+//            stack.push(max);
+//        }
+//    }
 
-        while (!tempStack.isEmpty()) {
-            stack.push(tempStack.pop());
-        }
-    }
+//    private static boolean gameLoop(int round) {
+////        Player1: A 3 7 8 10 J K Score: 0 Bag1 A 2 3 4 5 6 7 8 9 10 J Q K
+////Player2: 2 3 5 6 8 9 Q Score: 0 Bag2
+////1. selected value: 3
+//        System.out.print("Player1: ");
+////        printStack(player1);
+//        System.out.print("Player2: ");
+////        printStack(player2);
+//        return true;
+//    }
+//
+//    private static void printStack(Stack stack) {
+////        Stack tempStack = new Stack();
+//        while (!stack.isEmpty()) {
+//            Object element = stack.pop();
+//            System.out.print(element + " ");
+////            tempStack.push(element);
+//        }
+//
+//        while (!tempStack.isEmpty()) {
+//            stack.push(tempStack.pop());
+//        }
+//    }
 }
